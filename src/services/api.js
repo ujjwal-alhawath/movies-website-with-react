64bd736a544358edea6de3a8c5aafa91bbea9d37
ujjwal-alhawath <ujjwalahlawath@gmail.com>
2026-03-08 18:54:36 +0530

    Initial commit: Movie search app with React

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "f699409859c164547397c1064f4dacf2";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json(); 
+  return data.results;
+};
