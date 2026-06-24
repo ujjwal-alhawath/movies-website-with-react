1777c8e7e160e60d73bbbb5d28ead99499f68125
Ujjwal alhawath <ujjwalahlawath@gmail.com>
2026-06-24 11:35:36 +0530

    Use env variables for TMDB API key and update base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const API_KEY = "your api";
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = "https://api.tmdb.org/3";
 
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
